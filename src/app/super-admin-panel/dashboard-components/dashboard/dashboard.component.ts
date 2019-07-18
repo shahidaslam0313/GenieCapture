3ae085d47308514001abf111ad3c1eb57be72e41
shahidaslam0313 <shahid.aslam@brainplow.com>
2019-07-18 17:06:42 +0500

    adsfasdf

--- src/app/super-admin-panel/dashboard-components/dashboard/dashboard.component.ts
+++ src/app/super-admin-panel/dashboard-components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
     selector: 'dashboard-component',
     templateUrl: 'dashboard.component.html',
@@ -10,7 +10,6 @@ export class DashboardComponent implements OnInit {
     constructor() { }
 
     ngOnInit() { 
-
     }
 
 }
